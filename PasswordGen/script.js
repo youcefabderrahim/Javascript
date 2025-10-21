462fe5bde6782e69f18999486fb3efe96c633d61
Youcef <youcefcopy@gmail.com>
2025-10-21 15:01:15 +0100

    JS has been updated | added logic and the base structure of generating

--- PasswordGen/script.js
+++ PasswordGen/script.js
@@ -1,1 +1,33 @@
-// This is random password generator 
+// This is random password generator
+
+function generatPassword(params) {
+  // ! Declaring some consts
+  const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolsChars = "!@#$%^&*()_+-=";
+
+  // ! Declaring some vars
+  let allowedChars = "";
+  let password = "";
+
+  // ! We will string concatenate our lower case chars
+  allowedChars += includeLowerCase ? lowerCaseChars : "";
+  allowedChars += includeCapitalCase ? uperCaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolsChars : "";
+
+  console.log(allowedChars);
+}
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeCapitalCase = false;
+const includeNumbers = false;
+const includeSymbols = false;
+
+const password = generatPassword(passwordLength,
+                                includeLowerCase,
+                                includeCapitalCase,
+                                includeNumbers,
+                                includeSymbols
+);
