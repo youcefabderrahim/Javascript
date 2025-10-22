82f79ae317a6550460b4770c85f9939ec79c712e
Youcef <youcefcopy@gmail.com>
2025-10-22 18:06:25 +0100

    map() | cube function added

--- 2-functions/map.js
+++ 2-functions/map.js
@@ -6,6 +6,14 @@ const numbers = [1, 2, 3, 4, 5];
 const squares = numbers.map(square);
 console.log(squares);
 
+const cubes = numbers.map(cube);
+console.log(cubes);
+
+
 function square (element) {
     return Math.pow(element, 2);
+}
+
+function cube (element) {
+    return Math.pow(element, 3);
 }
