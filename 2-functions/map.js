d04a404e0d5611c3a1560faefa5fa1fcbb1b2a5d
Youcef <youcefcopy@gmail.com>
2025-10-22 18:03:41 +0100

    map() introduction

---
+++ 2-functions/map.js
@@ -0,0 +1,11 @@
+// ! .map() => accepts a callback and applies that function to each element
+// ? of an array than returns a new array (very similar to foreach)
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map(square);
+console.log(squares);
+
+function square (element) {
+    return Math.pow(element, 2);
+}
