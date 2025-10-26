a473facf71088e7b47685d2486253c8e11cd5494
Youcef <youcefcopy@gmail.com>
2025-10-26 10:29:27 +0100

    Dom manipulation | create and append and element

---
+++ DOM/index.js
@@ -0,0 +1,11 @@
+// I- Add an element to the page
+// There are two ways to add elements to the page
+// 1- select the element you want to change, or to append elements to, speaking of append
+// append vs appendChild, with appendchild you can only append elements such as div, and other tags
+// with append you can append all of those with string any pretty much anything
+const body = document.body;
+body.append(`Hello`);
+
+// ! II- Create an element and then you have to add it
+const div = document.createElement('div');
+body.append(div);
